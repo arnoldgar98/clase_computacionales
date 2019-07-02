234c6ab99dec3c1167c2fef5e7e30612fca1daf8
arnoldgar98 <as.garcia11@uniandes.edu.co>
2019-07-02 16:59:22 +0000

    Clase 1 semana 5

--- S5C1/S5C1Deriv.cpp
+++ S5C1/S5C1Deriv.cpp
@@ -1,10 +1,32 @@
 #include<iostream>
 using namespace std;
 
+double *derivada(){
+    
+}
+
 int main()
 {
+    int a;
+    int b;
+    int numpuntos;
+    cout<<"Ingrese el primer numero ";
+    cin>>a;
+    cout<<"Ingrese el segundo numero ";
+    cin>>b;
+    cout<<"Ingrese el numero de puntos ";
+    cin>>numpuntos;
     
-    double *point;
+    float *deriva;
+    deriva= new float[numpuntos];
+    deriva[0]=a;
+    deriva[numpuntos-1]=b;
+    double delta= (b-a)/(numpuntos-1);
+    int derivada;
+    for(int i=0; i<=numpuntos; i++)
+    {
+        derivada += delta*i;
+    }
     return 0;    
         
 }
