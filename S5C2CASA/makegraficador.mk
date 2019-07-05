6f8fb25e4f2adac487997f8c7dc3037f8a7948d0
arnoldgar98 <as.garcia11@uniandes.edu.co>
2019-07-05 13:36:53 +0000

    ultima version

--- S5C2CASA/makegraficador.mk
+++ S5C2CASA/makegraficador.mk
@@ -1,6 +1,6 @@
 euleryrunge.pdf: plotgrafica.py datos.dat datos.dat2
 	python plotgrafica.py
 datos.dat: GarciaArnoldS5C2casa.cpp
-datos.dat2: GarciaArnoldS5C2casa.cpp
-	./a.out    
+datos.dat2: GarciaArnoldS5C2casa.cpp 
 	g++ GarciaArnoldS5C2casa.cpp
+	./a.out   
