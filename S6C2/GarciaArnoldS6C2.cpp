250406ed4907b71cb5e6b3c85b7bc2e953dbb987
arnoldgar98 <as.garcia11@uniandes.edu.co>
2019-07-10 04:30:06 +0000

    otro

--- S6C2/GarciaArnoldS6C2.cpp
+++ S6C2/GarciaArnoldS6C2.cpp
@@ -8,16 +8,23 @@ int main()
     double b=0.1;
     double l=1.0;
     int puntos= 200;
-    double dx= (b-a)/puntos;
-    
     double dt=0.005;
     double c=300;
+    double dx= (b-a)/puntos;
+    double cons= c*(dx/dt);
+    ofstream outfile;
+    outfile.open("datos.dat"); 
+    
     double x[puntos];
-    double u[puntos];
     double upres[puntos];
-    double cons= c*(dt/dx);
+    double ufut[puntos];
+    //condiciones iniciales
+    upres[0]=0;
+    upres[puntos]=0;
+    ufut[0]=0;
+    ufut[puntos]=0;
     
-    
+    //construccion del presente
     for(int i=0;i<puntos/2;i++)
     {
         upres[i]= ((2*0.01)/l)*i*dt;
@@ -29,42 +36,52 @@ int main()
     for(int i=0; i<=puntos;i++)
     {
         x[i]=x[i-1]+dx;
-        cout<<x[i]<<";"<<upres[i]<<endl;
+        outfile<<x[i]<<";"<<upres[i]<<endl;
     }
+    outfile.close();
     
-    //condiciones iniciales
-    upres[0]=0;
-    upres[puntos]=0;
-    double ufut[puntos];
-    ufut[0]=0;
-    ufut[puntos]=0;
     
-    for(int i=1;i<puntos-1;i++)
+    //construccion del primero
+    //El primero se ve pero es muy pero muy pequeño, apartir de ese intento contruir los otros 
+    ofstream outfile1;
+    outfile1.open("datos2.dat"); 
+    for(int i=1;i<puntos;i++)
     {
-        ufut[i]= upres[i]+ ((cons*cons)/2)* (upres[i+1] -2*upres[i]+ upres[i-1]);
+        ufut[i]= upres[i]+ ((cons*cons)/2)* (upres[i+1]-2*upres[i]+upres[i-1]);
+        
     }
     
     double upas[puntos];
-    for(int i=1;i<puntos-1;i++)
+    double uprett[puntos];
+    for(int i=0;i<=puntos;i++)
     {
         upas[i]=upres[i];
-        upres[i]=ufut[i];
+        uprett[i]=ufut[i];
+        outfile1<<x[i]<<";"<<uprett[i]<<endl;
     }
+    outfile1.close();
     
-    int tiempo=100;
+    
+    //para los futuros
+    ofstream outfile3;
+    outfile3.open("datos3.dat");
+    int tiempo=10;
     for(int i=0; i<tiempo;i++)
     {
         for(int j=1;j<puntos;j++)
         {
-            ufut[i]=            
+            ufut[i]=(2.0*(1.0-(cons*cons)))*uprett[i] - upas[i] + (cons*cons)*(uprett[i+1]+uprett[i-1]);
+            
         }
         for(int b=0;b<puntos;b++)
         {
-            upas[b]=upres[b];
-            upres[b]=ufut[b];
+            upas[b]=uprett[b];
+            uprett[b]=ufut[b];
+            outfile3<<x[b]<<";"<<uprett[b]<<endl;
         }
         
     }
+    outfile3.close();
     
     
     return 0;
--- S6C2/graficador.py
+++ S6C2/graficador.py
@@ -5,9 +5,19 @@ datos= np.genfromtxt("datos.dat",delimiter=";")
 x=datos[:,0]
 upre=datos[:,1]
 
+datos2= np.genfromtxt("datos2.dat",delimiter=";")
+x2=datos2[:,0]
+upre2=datos2[:,1]
+
+datos3= np.genfromtxt("datos3.dat",delimiter=";")
+x3=datos3[:,0]
+upre3=datos3[:,1]
+
 plt.figure()
 plt.plot(x,upre)
+plt.plot(x3,upre3)
+plt.grid()
 plt.title("Ecuación del resorte ")
 plt.xlabel("Tiempo")
 plt.ylabel("Posición")
-plt.savefig("GarciaArnoldResorte.pdf")
+plt.savefig("otro")
