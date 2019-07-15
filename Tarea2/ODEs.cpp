7af925f5f0fedbf555428a54b71a7e5282630416
arnoldgar98 <as.garcia11@uniandes.edu.co>
2019-07-15 03:19:21 +0000

    Quinto2

--- Tarea2/ODEs.cpp
+++ Tarea2/ODEs.cpp
@@ -32,7 +32,7 @@ double dxeny(float ti, float yy, float vy)
 
 //primer método: Euler
 
-float euler()
+float euler(int fin, int ini, int puntos)
 {
     //creacion de arreglos
     double dif= 0.01;
@@ -42,7 +42,7 @@ float euler()
     y[0]=0.9772;
     vx[0]=-6.35;
     vy[0]=0.606;
-    dt=(b-a)/puntos;
+    dt=(fin-ini)/puntos;
     //desde 1, porque ya usamos la condicion inicial
     for(int i=1; i<puntos;i++)
     {
@@ -57,7 +57,7 @@ float euler()
 }
 
 //Segundo Método:Leap frog
-float leap_frog()
+float leap_frog(int fin, int ini, int puntos)
 {
     //creacion de arreglos
     float x[puntos], y[puntos], vy[puntos], vx[puntos], tiem[puntos];
@@ -66,7 +66,7 @@ float leap_frog()
     y[0]=0.9772;
     vx[0]=-6.35;
     vy[0]=0.606;
-    dt=(b-a)/puntos;
+    dt=(fin-ini)/puntos;
     //desde 1, porque ya usamos la condicion inicial
     for(int i=1; i<puntos;i++)
     {
@@ -80,43 +80,67 @@ float leap_frog()
     } 
 }
 //Tercer método: Runge-kutta 4orden
-float rungekutta()
+float rungekutta(int fin, int ini, int puntos)
 {
     double dif= 0.01;
     //creacion de arreglos
     float x[puntos], y[puntos], vy[puntos], vx[puntos], tiem[puntos];
-    float k1x,k2x,k3x,k4x,k1vx,k2vx,k3vx,k4vx;
-    float k1y,k2y,k3y,k4y,k1vy,k2vy,k3vy,k4vy;
+    float k1x,k2x,k3x,k4x,k1vx,k2vx,k3vx,k4vx,promex,promevx;
+    float k1y,k2y,k3y,k4y,k1vy,k2vy,k3vy,k4vy,promey,promevy;
     //inicializacion con condiciones del enunciado
     x[0]=0.1163;
     y[0]=0.9772;
     vx[0]=-6.35;
     vy[0]=0.606;
-    dt=(b-a)/puntos;
+    dt=(fin-ini)/puntos;
  
     for(int i=1;i<=puntos;i++)
     {
+        //Pasos para X
         k1x=dif*dxenx(tiem[i-1],x[i-1],vx[i-1]);
         k1vx=dif*dvenx(tiem[i-1],x[i-1],vx[i-1]);
             
         k2x=dif*dxenx(tiem[i-1]+0.5*dif, x[i-1]+0.5*k1x,vx[i-1]+0.5*k1vx);
-        k2vx=dif*dvenx(tiem[i-1]+0.5*dif, x[i-1]+0.5*k1x,vx[i-1]+0.5*k1vx);
+        k2vx=dif*dvenx(tiem[i-1]+0.5*dif, x[i-1]+0.5*k1x,vx[i-1]+0.5*k1x);
             
         k3x=dif*dxenx(tiem[i-1]+0.5*dif, x[i-1]+0.5*k2x,vx[i-1]+0.5*k2vx);
         k3vx=dif*dvenx(tiem[i-1]+0.5*dif,x[i-1]+0.5*k2x,vx[i-1]+0.5*k2x);
             
-        float k4x =delta*dx(t[i-1]+delta, x[i-1]+k3x, v[i-1]+ k3v);
-        float k4v =delta*dv(t[i-1]+delta, x[i-1]+k3x, v[i-1]+ k3x);
-        float promediox=(1.0/6.0)*(k1x+2.0*k2x+2.0*k3x +k4x);
-        float promediov=(1.0/6.0)*(k1v+2.0*k2v+2.0*k3v +k4v);
+        k4x =delta*dxenx(tiem[i-1]+dif, x[i-1]+k3x, vx[i-1]+ k3vx);
+        k4v =delta*dvenx(tiem[i-1]+dif, x[i-1]+k3x, vx[i-1]+ k3x);
+        
+        //Pasos para Y
+        k1y=dif*dxeny(tiem[i-1],y[i-1],vy[i-1]);
+        k1vy=dif*dveny(tiem[i-1],y[i-1],vy[i-1]);
             
-        tiem[i] = tiem[i-1] + delta;
-        x[i] = x[i-1] + promediox;
-        v[i] = v[i-1] + promediov;
+        k2y=dif*dxeny(tiem[i-1]+0.5*dif, x[i-1]+0.5*k1y,vy[i-1]+0.5*k1vy);
+        k2vy=dif*dveny(tiem[i-1]+0.5*dif, x[i-1]+0.5*k1y,vy[i-1]+0.5*k1y);
+            
+        k3y=dif*dxeny(tiem[i-1]+0.5*dif, y[i-1]+0.5*k2y,vy[i-1]+0.5*k2vy);
+        k3vy=dif*dveny(tiem[i-1]+0.5*dif,y[i-1]+0.5*k2y,vy[i-1]+0.5*k2y);
+            
+        k4y =delta*dxeny(tiem[i-1]+dif, y[i-1]+k3y, vy[i-1]+ k3vy);
+        k4vy =delta*dveny(tiem[i-1]+dif, y[i-1]+k3y, vy[i-1]+ k3y);
+        
+        //Paso final de rungekutta
+        promex=(1/6)*(k1x+2.0*k2x+2.0*k3x+k4x);
+        promevx=(1/6)*(k1vx+2.0*k2vx+2.0*k3vx+k4vx);
+            
+        promey=(1/6)*(k1y+2.0*k2y+2.0*k3y+k4y);
+        promevx=(1/6)*(k1vy+2.0*k2vy+2.0*k3vy+k4vy);
+        tiem[i] = tiem[i-1] + dif;
+        x[i] = x[i-1] + promex;
+        y[i] = y[i-1] + promey;
+        vx[i] = vx[i-1] + promevx;
+        vy[i] = vy[i-1] + promevy;
     }
     
 }
 int main()
 {
+    euler(20,0,100);
+    leap_frog(20,0,100);
+    rungekutta(20,0,100);
+        
     return 0;
 }
